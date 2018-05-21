4d9417cb5202e22e064722de731c06b0854f4fba
Alexandre Pedrecal <pedrecalc@gmail.com>
2018-05-21 20:38:50 -0300

    proxy trusted

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -11,7 +11,7 @@ module.exports = app => {
   app.get('/auth/google/callback', passport.authenticate('google'));
 
   app.get('/api/logout', (req, res) => {
-    res.logout();
+    req.logout();
     res.send(req.user);
   });
 
